4908aa9cf6e0041f02ad1abc33491577706ea02f
ZakaNaji <zakaria.naji@edu.uca.ma>
2024-05-18 17:57:41 +0100

    #08-2: we can target the specifi bean we want to apply the logic on using either name or type

--- src/main/java/org/znaji/shop/config/BeanCheckPostProcess.java
+++ src/main/java/org/znaji/shop/config/BeanCheckPostProcess.java
@@ -2,18 +2,23 @@ package org.znaji.shop.config;
 
 import org.springframework.beans.factory.config.BeanPostProcessor;
 import org.springframework.stereotype.Component;
+import org.znaji.shop.entity.Product;
 
 @Component
 public class BeanCheckPostProcess implements BeanPostProcessor {
     @Override
     public Object postProcessBeforeInitialization(Object bean, String beanName) {
-        System.out.println("Before initialization: " + beanName);
+        if (bean instanceof Product product) {
+            System.out.println("Before init procss - Product name: " + product.getName());
+        }
         return bean;
     }
 
     @Override
     public Object postProcessAfterInitialization(Object bean, String beanName) {
-        System.out.println("After initialization: " + beanName);
+        if (bean instanceof Product product) {
+            System.out.println("After init procss - Product name: " + product.getName());
+        }
         return bean;
     }
 }
