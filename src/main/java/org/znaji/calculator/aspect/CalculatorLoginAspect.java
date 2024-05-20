796f909e98a32d8a1e6084449175441399a0b6ed
ZakaNaji <zakaria.naji@edu.uca.ma>
2024-05-21 00:05:56 +0100

    #11-1: diff aspec anotations used: @Before will always be exe before the target is exe, @After will always exe after the methode is finished; @AfterReturning only if the methode finished withour error(can inject the return obj to this aspect); @AfterThrowing only if the methode finished with exception

--- src/main/java/org/znaji/calculator/aspect/CalculatorLoginAspect.java
+++ src/main/java/org/znaji/calculator/aspect/CalculatorLoginAspect.java
@@ -2,8 +2,7 @@ package org.znaji.calculator.aspect;
 
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.JoinPoint;
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.*;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -16,5 +15,24 @@ public class CalculatorLoginAspect {
         var methodName = joinPoint.getSignature().getName();
         var args = joinPoint.getArgs();
         log.info("The method {} is called with args {}", methodName, args);
+    }
+
+    @After("execution(* org.znaji.calculator.entity.*.*(..))")
+    public void logAfterStandardCalculatorAddMethod(JoinPoint joinPoint) {
+        var methodName = joinPoint.getSignature().getName();
+        log.info("The method {} is finished", methodName);
+    }
+
+    @AfterReturning(pointcut = "execution(* org.znaji.calculator.entity.*.*(..))", returning = "result")
+    public void logAfterReturningStandardCalculatorAddMethod(JoinPoint joinPoint, Object result) {
+        var methodName = joinPoint.getSignature().getName();
+        log.info("The method {} returns {}", methodName, result);
+    }
+
+
+    @AfterThrowing(pointcut = "execution(* org.znaji.calculator.entity.*.*(..))", throwing = "e")
+    public void logAfterThrowingStandardCalculatorAddMethod(JoinPoint joinPoint, Exception e) {
+        var methodName = joinPoint.getSignature().getName();
+        log.error("The method {} throws exception {}", methodName, e);
     }
 }
